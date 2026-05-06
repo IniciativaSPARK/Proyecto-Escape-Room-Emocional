d56fd9ea27abeedbd9f36110a54d880d6e5e26fe
Sofia <aracelysofiaperu@gmail.com>
2026-05-06 08:45:11 -0500

    setear serv

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,2 +1,17 @@
 //Aqui tendremos el codigo principal del backend
 //Importaremos dependencias y configuraremos el servidor
+
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send('Servidor Express funcionando')
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en https://localhost:${PORT}`);
+})
