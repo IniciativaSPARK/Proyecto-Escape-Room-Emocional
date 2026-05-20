5208a2e6310397c164cbf0dbcc600c94f6468990
kori <leonardo.chuquilin@usil.pe>
2026-05-20 00:58:58 -0500

    Restore fronted from main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
-// 
 import { createRoot } from 'react-dom/client'
-import './styles/base.css'
+import './index.css'
 import App from './App.jsx'
 
-//toma el componente llamado App y 
-// lo dibuja dentro del div root que
-// se encuentra en el index.html
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-
