70607e312bcd92ad2c3590390a21c7dd248402eb
leo <142282773+kori2565476@users.noreply.github.com>
2026-06-01 22:43:35 -0500

    Feature/leo developer (#9)
    * refactor: modularize styles and restructure fronted entry
    
    * feat: primer issue
    
    * fix : .env to backend
    
    * Restore fronted from main
    
    * refactor: align backend CRUD with PostgreSQL schema
    
    * feature: fix(backend): correccion de credenciales por username y verificacion de flujo con base de datos y Postman satisfactoria.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,2 +1,30 @@
-//Aqui tendremos el codigo principal del backend
-//Importaremos dependencias y configuraremos el servidor
+import express from 'express';
+
+import credentialsRoutes from './routes/credentialsRoutes.js';
+
+const app = express();
+
+app.use(express.json());
+
+// ======================================
+// API ROUTES
+// ======================================
+
+app.use('/api', credentialsRoutes);
+
+// ======================================
+// SERVER
+// ======================================
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+// any person can entrar por q no tenemos cors
+// cors : que dominions o ip pueden acceder
+// tipo de solicitudes
+
+// CORS permite o no tener http o https
+// 
